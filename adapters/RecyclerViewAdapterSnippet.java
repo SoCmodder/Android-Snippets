b73efe0847431a81be4b738d6de7dc7f2455883c
Mitch Miller <socmodder@gmail.com>
2016-12-19 10:19:00 -0500

    initial

---
+++ adapters/RecyclerViewAdapterSnippet.java
@@ -0,0 +1,46 @@
+public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+    private ArrayList<String> myDataSet;
+
+    // Provide a suitable constructor (depends on the kind of dataset)
+    public MyAdapter(ArrayList<String> myDataSet) {
+        this.myDataSet = myDataSet;
+    }
+
+    // Create new views (invoked by the layout manager)
+    @Override
+    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        // create a new view
+        View v = LayoutInflater.from(parent.getContext())
+                               .inflate(R.layout.my_layout, parent, false);
+        // set the view's size, margins, paddings and layout parameters
+        ViewHolder vh = new ViewHolder(v);
+        return vh;
+    }
+
+    // Replace the contents of a view (invoked by the layout manager)
+    @Override
+    public void onBindViewHolder(ViewHolder holder, int position) {
+        // - get element from your dataset at this position
+        // - replace the contents of the view with that element
+        //holder.mTextView.setText(mDataset[position]);
+
+    }
+
+    // Return the size of your dataset (invoked by the layout manager)
+    @Override
+    public int getItemCount() {
+        return mDataset.length;
+    }
+
+    // Provide a reference to the views for each data item
+    // Complex data items may need more than one view per item, and
+    // you provide access to all the views for a data item in a view holder
+    public static class ViewHolder extends RecyclerView.ViewHolder {
+        // each data item is just a string in this case
+        public ViewHolder(TextView v) {
+            super(v);
+            Butterknife.bind(this, v);
+        }
+    }
+}
+
